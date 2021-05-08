f1b11d450ed9bed4d80622403d724c8795901370
kubab96 <kubabaran12@gmail.com>
2021-05-08 18:59:42 +0200

    Zadanie - silnia

---
+++ src/silnia.js
@@ -0,0 +1,13 @@
+var liczby = [0, 1, 2, 10, 15, 20];
+for (var i = 0; i < liczby.length; i++) {
+    var liczba = liczby[i];
+    console.log('Silnia dla liczby', liczba, 'wynosi:');
+    var silnia = 1;
+    Silnia(liczba, silnia);
+    function Silnia(liczba, silnia) {
+        for (var z = 0; z > liczba; z++) {
+            silnia = silnia * z;
+        }
+        console.log(silnia);
+    }
+}
---
+++ src/silnia.ts
@@ -0,0 +1,16 @@
+const liczby: Array<number> = [0,1,2,3,10,15,20];
+
+for(let i = 0; i < liczby.length; i++){
+    const liczba: number = liczby[i];
+    console.log('Silnia dla liczby', liczba,'wynosi:')
+    const silnia: number = 1;
+    
+    Silnia(liczba, silnia)
+    
+    function Silnia(liczba:number, silnia: number){
+        for(let z = liczba; z > 1; z--){
+            silnia = silnia * z;
+        }
+        console.log(silnia)
+    }
+}
