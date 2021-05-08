4042c85dee309a599bff3bb0e5de9214f0823a22
kubab96 <kubabaran12@gmail.com>
2021-05-08 18:05:24 +0200

    Zadanie - ciąg fibonacciego

---
+++ src/fibonacci.ts
@@ -0,0 +1,21 @@
+const liczby: Array<number> = [1,2,10,15,20];
+
+for(let i = 0; i < liczby.length; i++){
+    const liczba: number = liczby[i];
+    const liczba2: number = liczba;
+
+    console.log('Kolejne liczby ciągu dla', liczba)
+
+    Ciag(5,liczba,liczba2)
+    
+    // ilosc = ilosc powtorzen ciagu
+    function Ciag(ilosc: number, liczba:number, liczba2: number){
+        for(let z = 0; z < ilosc; z++){
+            const wynik: number = liczba + liczba2;
+            console.log(wynik)
+            liczba2 = wynik;
+            liczba = wynik - liczba;
+        }
+        console.log('----------------------------')
+    }
+}
